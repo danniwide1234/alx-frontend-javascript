52e2f658493ae2013756f742ea3d2d40d1d6b9a1
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2024-07-25 18:31:36 +0100

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,14 +3,15 @@
 
 import Car from './10-car';
 
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
     super(brand, motor, color);
     this._range = range;
   }
 
-  cloneCar() {
-    const Spec = super.constructor[Symbol.species];
-    return new Spec();
+  static get [Symbol.species]() {
+    return Car;
   }
 }
+
+export default EVCar;
